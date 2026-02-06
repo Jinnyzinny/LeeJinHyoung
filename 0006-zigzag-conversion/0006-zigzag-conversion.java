f67ae69a5e50658e6c7c5a9312963582792b6fcb
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2026-02-06 16:12:20 +0900

    Time: 99 ms (5.23%), Space: 47.4 MB (17.58%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] row=new StringBuilder[numRows];
+        for(int i=0;i<row.length;i++){
+            row[i]=new StringBuilder();
+        }
+        int r=0;
+        boolean isDown=true;
+        
+        if(numRows==1){
+            return s;
+        }
+
+        String[] sSplit=s.split("");
+        for(String str:sSplit){
+            if(isDown){
+                row[r].append(str);
+                if(r==numRows-1){
+                    isDown=false;
+                    r--;
+                } else{
+                    r++;
+                }
+            } else{
+                row[r].append(str);
+                if(r==0){
+                    isDown=true;
+                    r++;
+                } else{
+                    r--;
+                }
+            }
+        }
+        StringBuilder answer=new StringBuilder();
+        for(int i=0;i<numRows;i++){
+            answer.append(row[i]);
+            System.out.println(row[i]);
+        }
+        return answer.toString();
+    }
+}
