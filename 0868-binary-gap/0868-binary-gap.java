9d2b60f4fbf7e40d8cccc6ab7c7736dd4c9e0561
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2026-02-22 12:03:59 +0900

    Time: 8 ms (3.15%), Space: 42.6 MB (14.06%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binaryGap(int n) {
+        int answer=0;
+        char binary[]=Integer.toBinaryString(n).toCharArray();
+
+        for(int i=0;i<binary.length;i++){
+            if(binary[i]=='0')
+                continue;
+            for(int j=i+1;j<binary.length;j++){
+                if(binary[j]=='1'){
+                    System.out.println(i+" "+j);
+                    answer=Math.max(answer,j-i);
+                    i=j-1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
