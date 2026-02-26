fb5f24c098fef67a126564253feb0bc23e096bab
이진형 <86180422+Jinnyzinny@users.noreply.github.com>
2026-02-26 11:10:23 +0900

    Time: 2 ms (40.12%), Space: 43.1 MB (25.07%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,41 +1,35 @@
 class Solution {
-
-    private void divideByTwo(StringBuilder s) {
+    private void divide(StringBuilder s) {
         s.deleteCharAt(s.length() - 1);
     }
-
-    private void addOne(StringBuilder s) {
-        int i = s.length() - 1;
+    private void add(StringBuilder s) {
+        int index = s.length() - 1;
 
         // Iterating while the character is 1 and changing to 0
-        while (i >= 0 && s.charAt(i) != '0') {
-            s.setCharAt(i, '0');
-            i--;
+        while (index >= 0 && s.charAt(index) != '0') {
+            s.setCharAt(index, '0');
+            index--;
         }
 
-        if (i < 0) {
+        if (index < 0) {
             s.insert(0, '1');
         } else {
-            s.setCharAt(i, '1');
+            s.setCharAt(index, '1');
         }
     }
 
     public int numSteps(String s) {
-        StringBuilder str = new StringBuilder(s);
+        StringBuilder bin=new StringBuilder(s);
 
-        int operations = 0;
-        while (str.length() > 1) {
-            int N = str.length();
-
-            if (str.charAt(N - 1) == '0') {
-                divideByTwo(str);
-            } else {
-                addOne(str);
+        int answer=0;
+        while(bin.length()>1){
+            if(bin.charAt(bin.length()-1)=='0'){
+                divide(bin);
+            } else if(bin.charAt(bin.length()-1)=='1'){
+                add(bin);
             }
-
-            operations++;
+            answer++;
         }
-
-        return operations;
+        return answer;
     }
 }
